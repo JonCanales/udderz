48dd6d1356b4e364326745bc9bc4d3ea83b2729d
Jonathan <andriodjonathan1@gmail.com>
2021-02-02 12:51:05 -0500

    Added Sound

--- udderz/ViewController.swift
+++ udderz/ViewController.swift
@@ -4,16 +4,41 @@
 //
 //  Created by Jonathan Canales on 1/28/21.
 //
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
-
+    var player: AVAudioPlayer?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func playSound(_ sender: Any) {
+        
+        soundPlayer()
+    }
+    func soundPlayer() {
+        guard let url = Bundle.main.url(forResource: "Alarm 2", withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
 
